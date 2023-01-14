fafe0b935a5f2cff260cf9d88a9cf5a950670401
Austin Perrine <akperrine@gmail.com>
2023-01-14 15:11:48 -0700

    create Redux store and listSlice

---
+++ src/app/features/list/listSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const listSlice = createSlice({
+  name: "list",
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    addToList: (state, action) => {
+      state.value = [...state.value, action.payload];
+    },
+    removeFromList: (state, action) => {
+      // Do I need to check for ID on this?
+      state.value = state.value.filter((item) => item !== action.payload);
+    },
+  },
+});
